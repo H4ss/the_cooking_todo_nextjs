f65c2ec58121e1c669ff37819830f27f6296ffcc
hass <hassoune.liban77@outlook.com>
2022-02-19 21:58:31 +0100

    [ADD] importing some react function to query data from fb

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,40 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { firestore } from '../firebase/clientApp';
+
+import {collection,QueryDocumentSnapshot,DocumentData,query,where,limit,getDocs} from "@firebase/firestore";
+
+const todosCollection = collection(firestore,'todos');
+import { useEffect, useState } from 'react';
+const [todos,setTodos] = useState<QueryDocumentSnapshot<DocumentData>[]>([]);
+const [loading,setLoading] = useState<boolean>(true);
+
+
+const getTodos = async () => {
+  // construct a query to get up to 10 undone todos 
+  const todosQuery = query(todosCollection,where('done','==',false),limit(10));
+  // get the todos
+  const querySnapshot = await getDocs(todosQuery);
+  
+  // map through todos adding them to an array
+  const result: QueryDocumentSnapshot<DocumentData>[] = [];
+  querySnapshot.forEach((snapshot) => {
+  result.push(snapshot);
+  });
+  // set it to state
+  setTodos(result);
+};
+
+useEffect( () => {
+  // get the todos
+  getTodos();
+  // reset loading
+  setTimeout( () => {
+    setLoading(false);
+  },2000)
+},[]);
+
 
 const Home: NextPage = () => {
   return (
@@ -17,6 +51,35 @@ const Home: NextPage = () => {
      <h1 className={styles.title}>
      Todos app
      </h1>
+
+     <div className={styles.grid}>
+{
+  loading ? (
+   <div className={styles.card}>
+    <h2>Loading</h2>
+   </div>
+  ): 
+  todos.length === 0 ? (
+   <div className={styles.card}>
+    <h2>No undone todos</h2>
+    <p>Consider adding a todo from <a href="/add-todo">here</a></p>
+   </div>
+  ) : (
+   todos.map((todo) => {
+    return (
+     <div className={styles.card}>
+      <h2>{todo.data.arguments['title']}</h2>
+      <p>{todo.data.arguments['description']}</p>
+      <div className={styles.cardActions}>
+      <button type="button">Mark as done</button>
+      <button type="button">Delete</button>
+      </div>
+     </div>
+    )
+   })
+  )
+}
+</div> 
 
      {/* hopefully, data will arrive here */}
 
