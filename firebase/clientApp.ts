d9a8593debe393008fbc65bd699dcc74fc9338eb
hass <hassoune.liban77@outlook.com>
2022-02-19 21:55:02 +0100

    [ADD] setting up the skeleton (style, firebase variable...)

--- firebase/clientApp.ts
+++ firebase/clientApp.ts
@@ -1,4 +1,5 @@
 import {initializeApp} from 'firebase/app';
+import {getFirestore} from 'firebase/firestore';
 
 initializeApp({
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -8,3 +9,7 @@ initializeApp({
     messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 });
+
+const firestore = getFirestore();
+
+export {firestore};
