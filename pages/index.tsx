ec5020c09b19724d3033928c9e26fcc7d03b713b
hass <hassoune.liban77@outlook.com>
2022-02-21 12:01:17 +0100

    [FIX] now can call the args without the strict mode

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,38 +6,39 @@ import { firestore } from '../firebase/clientApp';
 
 import {collection,QueryDocumentSnapshot,DocumentData,query,where,limit,getDocs} from "@firebase/firestore";
 
-const todosCollection = collection(firestore,'todos');
 import { useEffect, useState } from 'react';
-const [todos,setTodos] = useState<QueryDocumentSnapshot<DocumentData>[]>([]);
-const [loading,setLoading] = useState<boolean>(true);
 
-
-const getTodos = async () => {
-  // construct a query to get up to 10 undone todos 
-  const todosQuery = query(todosCollection,where('done','==',false),limit(10));
-  // get the todos
-  const querySnapshot = await getDocs(todosQuery);
-  
-  // map through todos adding them to an array
-  const result: QueryDocumentSnapshot<DocumentData>[] = [];
-  querySnapshot.forEach((snapshot) => {
-  result.push(snapshot);
-  });
-  // set it to state
-  setTodos(result);
-};
-
-useEffect( () => {
-  // get the todos
-  getTodos();
-  // reset loading
-  setTimeout( () => {
-    setLoading(false);
-  },2000)
-},[]);
 
 
 const Home: NextPage = () => {
+  
+  const [todos,setTodos] = useState<QueryDocumentSnapshot<DocumentData>[]>([]);
+  const [loading,setLoading] = useState<boolean>(true);
+  
+  useEffect( () => {
+    getTodos();
+    // reset loading
+    setTimeout( () => {
+      setLoading(false);
+    },2000)
+  },[]);
+  
+  const todosCollection = collection(firestore,'todos');
+
+  const getTodos = async () => {
+    const todosQuery = query(todosCollection,where('done','==',false),limit(10));
+    const querySnapshot = await getDocs(todosQuery);
+    
+    // map through todos adding them to an array
+    const result: QueryDocumentSnapshot<DocumentData>[] = [];
+    querySnapshot.forEach((snapshot) => {
+    result.push(snapshot);
+    });
+
+    setTodos(result);
+  };
+  
+
   return (
     <div className={styles.container}>
    <Head>
@@ -53,46 +54,43 @@ const Home: NextPage = () => {
      </h1>
 
      <div className={styles.grid}>
-{
-  loading ? (
-   <div className={styles.card}>
-    <h2>Loading</h2>
-   </div>
-  ): 
-  todos.length === 0 ? (
-   <div className={styles.card}>
-    <h2>No undone todos</h2>
-    <p>Consider adding a todo from <a href="/add-todo">here</a></p>
-   </div>
-  ) : (
-   todos.map((todo) => {
-    return (
-     <div className={styles.card}>
-      <h2>{todo.data.arguments['title']}</h2>
-      <p>{todo.data.arguments['description']}</p>
-      <div className={styles.cardActions}>
-      <button type="button">Mark as done</button>
-      <button type="button">Delete</button>
-      </div>
-     </div>
-    )
-   })
-  )
-}
+      {
+        loading ? (
+         <div className={styles.card}>
+          <h2>Loading</h2>
+         </div>
+        ): 
+        todos.length === 0 ? (
+          // console.log(todos),
+         <div className={styles.card}>
+          <h2>No undone todos</h2>
+          <p>Consider adding a todo from <a href="/add-todo">here</a></p>
+          {/* {console.log(todos)} */}
+         </div>
+        ) : (
+         todos.map((todo) => {
+          return (
+           <div className={styles.card}>
+            <h2>{todo.data().title}</h2>
+            <p>{todo.data().description}</p>
+            {/* <h2>{todo.data.arguments['title']}</h2>
+            <p>{todo.data.arguments['description']}</p> */}
+            <div className={styles.cardActions}>
+            <button type="button">Mark as done</button>
+            <button type="button">Delete</button>
+            </div>
+           </div>
+          )
+         })
+        )
+      }
 </div> 
 
      {/* hopefully, data will arrive here */}
 
    </main>
 
-   <footer className={styles.footer}>
-     <a
-     href="#"
-     rel="noopener noreferrer"
-     >
-     Todos app
-     </a>
-   </footer>
+
    </div>
   )
 }
